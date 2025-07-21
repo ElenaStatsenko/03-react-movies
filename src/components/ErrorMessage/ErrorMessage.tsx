c96502c3d1125a5a190adebce5ca6ec684a6973c
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2025-07-21 19:55:21 +0300

    отредактировала лоудер и ерор

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -6,8 +6,7 @@ export default function ErrorMessage({ isError }: ErrorMessageProps) {
   if (!isError) return null;
   return (
     <p className={css.text}>
-      {" "}
-      {isError}There was an error, please try again...
+      There was an error, please try again...
     </p>
   );
 }
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,12 +1,8 @@
 import css from "./Loader.module.css";
-interface LoaderProps{
-isLoading:boolean;
+interface LoaderProps {
+  isLoading: boolean;
 }
-export default function Loader({isLoading}: LoaderProps) {
-     if (!isLoading) return null;
-    return (
-        <p className={css.text}>{isLoading}Loading movies, please wait...</p>
-    )
+export default function Loader({ isLoading }: LoaderProps) {
+  if (!isLoading) return null;
+  return <p className={css.text}>Loading movies, please wait...</p>;
 }
-
-  
